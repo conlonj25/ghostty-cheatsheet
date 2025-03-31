43ff2ff56baf769fa3d81d19c0fa71c875450d16
James Conlon <conlonj25@gmail.com>
2025-03-31 18:00:00 +0200

    fix lint errors

--- src/ghostty-cheatsheet.tsx
+++ src/ghostty-cheatsheet.tsx
@@ -3,18 +3,14 @@ import shortcuts from "./shortcuts";
 
 export default function Command() {
   const platform: NodeJS.Platform = process.platform;
-  const os = platform === 'darwin' ? 'macOS' : 'Windows/Linux';
+  const os = platform === "darwin" ? "macOS" : "Windows/Linux";
 
   return (
     <List>
-      {Object.keys(shortcuts).map(key => (
+      {Object.keys(shortcuts).map((key) => (
         <List.Section key={key} title={key}>
-          {shortcuts[key].map(item => (
-            <List.Item
-            key={item.action}
-            title={item.action}
-            subtitle={item[os]}
-          />
+          {shortcuts[key].map((item) => (
+            <List.Item key={item.action} title={item.action} subtitle={item[os]} />
           ))}
         </List.Section>
       ))}
